ff3ddc99cec8eaff3b819ce9de727f1b96fd8d79
rahulkuma9503-coder <rahulkuma9503@gmail.com>
2025-11-16 15:47:23 +0530

    Add files via upload

--- RAUSHAN/modules/spam/replyraid.py
+++ RAUSHAN/modules/spam/replyraid.py
@@ -22,64 +22,25 @@ from RAUSHAN.modules.basic.profile import extract_user, extract_user_and_reason
 SUDO_USERS = SUDO_USER
 RAIDS = []
 
-    filters.command(["pspam"], ".") & (filters.me | filters.user(SUDO_USER))
+@Client.on_message(
+    filters.command(["pornspam"], ".") & (filters.me | filters.user(SUDO_USER))
 )
-async def raid(xspam: Client, e: Message):  
-      RAUSHAN = "".join(e.text.split(maxsplit=1)[1:]).split(" ", 2)
-      if len(RAUSHAN) == 2:
-          counts = int(RAUSHAN[0])
-          if int(e.chat.id) in GROUP:
-               return await e.reply_text("**Sorry !! i Can't Spam Here.**")
-          ok = await xspam.get_users(RAUSHAN[1])
-          id = ok.id
-#          try:
-#              userz = await xspam.get_users(id)
-#          except:
-#              await e.reply(f"`404 : User Doesn't Exists In This Chat !`")
-#              return #remove # to enable this
-          if int(id) in VERIFIED_USERS:
-                text = f"Chal Chal baap Ko mat sikha"
-                await e.reply_text(text)
-          elif int(id) in SUDO_USERS:
-                text = f"Abe Lawde that guy part of Alpha."
-                await e.reply_text(text)
-          else:
-              fname = ok.first_name
-              mention = f"[{fname}](tg://user?id={id})"
-              for _ in range(counts):
-                    reply = choice(PORN)
-                    msg = f"{mention} {reply}"
-                    await xspam.send_message(e.chat.id, msg)
-                    await asyncio.sleep(0.10)
-      elif e.reply_to_message:
-          msg_id = e.reply_to_message.from_user.id
-          counts = int(RAUSHAN[0])
-          if int(e.chat.id) in GROUP:
-               return await e.reply_text("**Sorry !! i Can't Spam Here.**")
-          user_id = e.reply_to_message.from_user.id
-          ok = await xspam.get_users(user_id)
-          id = ok.id
-          try:
-              userz = await xspam.get_users(id)
-          except:
-              await e.reply(f"`404 : User Doesn't Exists In This Chat !`")
-              return
-          if int(id) in VERIFIED_USERS:
-                text = f"Chal Chal baap Ko mat sikha"
-                await e.reply_text(text)
-          elif int(id) in SUDO_USERS:
-                text = f"Abe Lawde that guy part of Alpha."
-                await e.reply_text(text)
-          else:
-              fname = ok.first_name
-              mention = f"[{fname}](tg://user?id={id})"
-              for _ in range(counts):
-                    reply = choice(RAID)
-                    msg = f"{mention} {reply}"
-                    await xspam.send_message(e.chat.id, msg)
-                    await asyncio.sleep(0.10)
-      else:
-          await e.reply_text("Usage: .raid count username")
+async def pornspam(xspam: Client, e: Message): 
+    counts = e.command[0]
+    if not counts:
+        return await e.reply_text(usage)
+    if int(e.chat.id) in GROUP:
+         return await e.reply_text("**Sorry !! i Can't Spam Here.**")
+    kkk = "**#Pornspam**"
+    count = int(counts)
+    for _ in range(count):
+         prn = choice(PORM)
+         if ".jpg" in prn or ".png" in prn:
+              await xspam.send_photo(e.chat.id, prn, caption=kkk)
+              await asyncio.sleep(0.4)
+         if ".mp4" in prn or ".MP4," in prn:
+              await xspam.send_video(e.chat.id, prn, caption=kkk)
+              await asyncio.sleep(0.4)
 
 @Client.on_message(
     filters.command(["hang"], ".") & (filters.me | filters.user(SUDO_USER))
@@ -166,43 +127,6 @@ add_command_help(
         [".hang", "Make telegram hang."],
     ],
 )
-
-@Client.on_message(
-    filters.command(["replyraid"], ".") & (filters.me | filters.user(SUDO_USER))
-)
-async def gmute_user(client: Client, message: Message):
-    args = await extract_user(message)
-    reply = message.reply_to_message
-    ex = await message.edit_text("`Processing...`")
-    if args:
-        try:
-            user = await client.get_users(args)
-        except Exception:
-            await ex.edit(f"`Please specify a valid user!`")
-            return
-    elif reply:
-        user_id = reply.from_user.id
-        user = await client.get_users(user_id)
-    else:
-        await ex.edit(f"`Please specify a valid user!`")
-        return
-    if user.id == client.me.id:
-        return await ex.edit("**Okay Sure.. 🐽**")
-    elif user.id == SUDO_USERS:
-        return await ex.edit("**Okay But Failed Because this user in sudos.. 🐽**")
-    elif user.id == VERIFIED_USERS:
-        return await ex.edit("**Chal Chal Baap ko Mat sikha.. 🐽**")
-    try:
-        if user.id in (await get_rraid_users()):
-           await ex.edit("Replyraid is activated on this user")
-           return
-        await rraid_user(user.id)
-        RAIDS.append(user.id)
-        await ex.edit(f"[{user.first_name}](tg://user?id={user.id}) Activated ReplyRaid!")
-    except Exception as e:
-        await ex.edit(f"**ERROR:** `{e}`")
-        return
-
 
 @Client.on_message(
     filters.command(["dreplyraid"], ".") & (filters.me | filters.user(SUDO_USER))
